d1f784e27f82df3dd80e010b538175cf5d9c96aa
laerer5cmysen <99287788+laerer5cmysen@users.noreply.github.com>
2022-02-16 12:16:54 +0100

    Oppdater pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,27 +3,31 @@ input.onButtonPressed(Button.A, function () {
     radio.sendNumber(0)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    basic.showNumber(2)
-    radio.sendNumber(2)
+    basic.showNumber(3)
+    radio.sendNumber(3)
 })
 input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(8)
+    radio.sendNumber(8)
+})
+input.onGesture(Gesture.LogoUp, function () {
     basic.showNumber(6)
     radio.sendNumber(6)
 })
-input.onGesture(Gesture.LogoUp, function () {
-    basic.showNumber(5)
-    radio.sendNumber(5)
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(7)
+    radio.sendNumber(7)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(1)
     radio.sendNumber(1)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    basic.showNumber(3)
-    radio.sendNumber(3)
-})
-input.onGesture(Gesture.LogoDown, function () {
     basic.showNumber(4)
     radio.sendNumber(4)
 })
-radio.setGroup(1)
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(5)
+    radio.sendNumber(5)
+})
+radio.setGroup(10)
